8d67709508f87fecdcf20ba15be4ae175aa523ea
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-10-29 18:57:21 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -27,6 +27,8 @@ class Snake:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.dirn = self.body[0].dirn
+
         
 # ------------------------------------------------------------------------------------------------------------------------- #
 class Cell:
@@ -84,19 +86,19 @@ def main():
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_UP]:
-#             cell.y -= rez
+            # cell.y -= rez
             cell.dirn = 'up'
 
         elif keys[pygame.K_DOWN]:
-#             cell.y += rez
+            # cell.y += rez
             cell.dirn = 'down'
 
         elif keys[pygame.K_LEFT]:
-#             cell.x -= rez
+            # cell.x -= rez
             cell.dirn = 'left'
 
         elif keys[pygame.K_RIGHT]:
-#             cell.x += rez
+            # cell.x += rez
             cell.dirn = 'right'
 
         for i in range(1, width // rez):
